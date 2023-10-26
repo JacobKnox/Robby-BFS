611a4c135f8d49809eb448f5bf86f926cfa570e0
Jacob Knox <jacobknoxa@gmail.com>
2023-10-26 12:32:14 -0400

    Complete everything but BFS

--- robby_search.py
+++ robby_search.py
@@ -32,23 +32,27 @@ parser.add_argument('-v', '--verbose',
                     action=argparse.BooleanOptionalAction)
 
 
-def main(file, actions, battery, verbose):
+def main(file: str, actions: str, battery: int, verbose: bool):
     # Read world parameters (size, location of Robby, and contents) from file
     # ***EDIT CODE HERE***
-    rows, cols = 1, 1
-    r0, c0 = 0, 0
-    contents = 'E'
+    lines = open(file, 'r').readlines()
+    rows, cols = [int(char) for char in lines[0].strip().split(' ')]
+    r0, c0 = [int(char) for char in lines[1].strip().split(' ')]
+    contents = ''.join([line.strip() for line in lines[2:]]).replace('.', 'E')
 
     # Create Robby's world
     # ***EDIT CODE HERE***
     rw = World(rows, cols)
     rw.graphicsOn()
+    rw.load(contents)
+    rw.goto(r0, c0)
+    rw.setFullBattery(battery)
 
     # Play in Robby's world
     path = ''
     while True:
         # Check to see if Robby has picked up all the cans
-        if False:  # ***EDIT CODE HERE***
+        if rw.getCansRemaining() <= 0:  # ***EDIT CODE HERE***
             rw.graphicsOff("Robby wins!")
 
         # Handle key presses
@@ -91,10 +95,19 @@ def main(file, actions, battery, verbose):
 
                 # ***EDIT CODE HERE***
                 for action in path:
-                    pass
+                    if action == "N":
+                        rw.north()
+                    elif action == "S":
+                        rw.south()
+                    elif action == "E":
+                        rw.east()
+                    elif action == "W":
+                        rw.west()
+                    elif action == "G":
+                        rw.grab()
 
 
-def bfs(rw, state, actions, verbose=False):
+def bfs(rw: World, state: str, actions: str, verbose=False) -> str:
     '''Perform breadth-first search on the world state given an ordered string of actions to check (e.g. 'GNESW').'''
     # ***EDIT CODE HERE***
     cnt = 0  # counter to see how long the search took
@@ -106,20 +119,26 @@ def bfs(rw, state, actions, verbose=False):
     return path
 
 
-def issolved(rw, state, path):
+def issolved(rw: World, state: str, path: str) -> bool:
     '''Check whether a series of actions (path) taken in Robby's world results in a solved problem.'''
-    rows, cols = rw.numRows, rw.numCols  # size of the world
     row, col = rw.getCurrentPosition()  # Robby's current (starting) position
     state = list(state)  # convert the string to a list so we can update it
-    battery = rw.fullBattery
+
+    if not isvalid(rw, state, path):
+        return False
 
     # ***EDIT CODE HERE***
     for action in path:
-        pass
-
-        # Did Robby run out of battery?
-        if battery <= 0:
-            return False
+        if action == "N":
+            row += 1
+        elif action == "S":
+            row -= 1
+        elif action == "E":
+            col += 1
+        elif action == "W":
+            col -= 1
+        elif action == "G":
+            state[row][col] = "E"
 
         # Did Robby grab all the cans?
         if state.count("C") == 0:
@@ -128,7 +147,7 @@ def issolved(rw, state, path):
     return False  # if we made it this far, we did not complete the goal
 
 
-def isvalid(rw, state, path):
+def isvalid(rw: World, state: str, path: str) -> bool:
     '''Check whether a series of actions (path) taken in Robby's world is valid.'''
     rows, cols = rw.numRows, rw.numCols  # size of the maze
     row, col = rw.getCurrentPosition()  # robby's current (starting) position
@@ -138,18 +157,33 @@ def isvalid(rw, state, path):
 
     # ***EDIT CODE HERE***
     for action in path:
-        pass
+        battery -= 1
+
+        if action == "N":
+            row += 1
+        elif action == "S":
+            row -= 1
+        elif action == "E":
+            col += 1
+        elif action == "W":
+            col -= 1
+        elif action == "G":
+            item = state[row][col]
+            if item == "B":
+                battery = rw.fullBattery
+            if item != 'W':
+                state[row][col] = "E"
 
         # Path is invalid if Robby has run out of battery
-        if False:  # ***EDIT CODE HERE***
+        if battery <= 0:  # ***EDIT CODE HERE***
             return False
 
         # Path is invalid if Robby's goes "out of bounds"
-        if False:  # ***EDIT CODE HERE***
+        if row < 0 or row >= rows or col < 0 or col >= cols:  # ***EDIT CODE HERE***
             return False
 
         # Path is invalid if Robby runs into a wall
-        if False:  # ***EDIT CODE HERE***
+        if state[row][col] == 'W':  # ***EDIT CODE HERE***
             return False
 
         # Path is invalid if robby repeats a state in memory
@@ -163,4 +197,4 @@ def isvalid(rw, state, path):
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    main(args.file, args.actions, args.battery)
+    main(args.file, args.actions, args.battery, args.verbose)
